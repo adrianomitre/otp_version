654a09ba8f5eaaa150154933dacf60f5e0d3e6c6
Adriano Mitre <adriano.mitre@gmail.com>
2020-09-12 14:23:10 -0300

    add dep dialyxir

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule OtpVersion.MixProject do
       version: "0.1.0",
       elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
-      deps: []
+      deps: deps()
     ]
+  end
+
+  defp deps do
+    if Version.match?(System.version(), ">= 1.6.0") do
+      [{:dialyxir, "~> 1.0"}]
+    end
   end
 end
