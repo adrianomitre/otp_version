aa0d49192a6c72e7e5635d6e98c494ae8a30dbaf
Adriano Mitre <adriano.mitre@gmail.com>
2020-09-12 16:18:35 -0300

    improve tests

--- test/otp_version_test.exs
+++ test/otp_version_test.exs
@@ -5,12 +5,17 @@ defmodule OtpVersionTest do
   import OtpVersion, only: [otp_version: 0, otp_version: 1]
 
   test "otp_release/1" do
+    if Version.match?(System.build_info().version, ">= 1.3.0") do
+      assert System.otp_release() == otp_version(:major_only)
+    end
+
     assert otp_version() == otp_version(:major_only)
+    assert {:ok, version} = Version.parse(otp_version(:semantic_versioning_scheme))
 
     Enum.each(
       [
         major_only: 1..1,
-        otp_version_scheme: 2..6,
+        otp_version_scheme: 1..6,
         semantic_versioning_scheme: 3..3
       ],
       fn {version_scheme, expected_components_range} ->
