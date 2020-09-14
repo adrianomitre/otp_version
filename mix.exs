5ecaf4e5977626ecdbe4235d6853c58229d0c3fe
Adriano Mitre <adriano.mitre@gmail.com>
2020-09-13 23:59:28 -0300

    bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -1,6 +1,6 @@
-defmodule OtpVersion.MixProject do
+defmodule OTPVersion.MixProject do
   use Mix.Project
-  @version "0.1.0"
+  @version "0.1.1"
   @repo_url "https://github.com/adrianomitre/otp_version"
 
   def project do
